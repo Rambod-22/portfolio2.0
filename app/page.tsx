4929afe5fc842f8af44776f207b795f141b517e7
Rambod Zafarmoghaddam <rambodzafar@Rambods-MacBook-Pro.local>
2024-02-23 21:33:19 -0500

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default async function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
-        Rambod's Portfolio
+        {pageInfo?.name} Portfolio
       </Head>
 
       <Header socials={socials}/>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,7 +12,7 @@ function Projects({ projects}: Props) {
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
             <div className='scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
                 {projects.map((project, i) => (
-                    <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+                    <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                         <motion.img className='h-96' initial={{y:-300, opacity: 0}} transition={{duration: 1.2}} whileInView={{opacity: 1, y:0}} viewport={{once:true}} src={urlFor(project?.image).url()} alt="" />
                         <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                             <h4 className='text-4xl font-semibold text-center'><span className='underline decoration-[#F7AB0A]/50'>Case Study {i + 1} of {projects.length}:</span>{" "}{project.title}</h4>
