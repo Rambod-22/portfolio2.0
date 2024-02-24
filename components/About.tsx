f172c91af9c3318a7ed5ccff4a9e2d9699301a5a
Rambod Zafarmoghaddam <rambodzafar@Rambods-MacBook-Pro.local>
2024-02-24 18:19:26 -0500

    changes

--- components/About.tsx
+++ components/About.tsx
@@ -13,7 +13,7 @@ function About({ pageInfo, isVisible }: Props) {
     return (
     //     <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration:1.5}} className='h-screen flex flex-col relative text-center md:text-left 
     // md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <motion.div initial={{opacity: 1}} animate={{opacity: 1}} transition={{duration:1.5}} className='h-screen flex flex-col relative text-center md:text-left 
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration:1.5}} className='h-screen flex flex-col relative text-center md:text-left 
     md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
             <motion.img initial={{x:0, opacity: 1}} src={urlFor(pageInfo?.profilePic).url()} transition={{duration: 1.2}} animate={{opacity: 1, x:0}} viewport={{once:true}} className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"   />
