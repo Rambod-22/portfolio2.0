3c5f40cd5370e6afd49827375ae7d71f2ecc9344
Rambod Zafarmoghaddam <rambodzafar@Rambods-MacBook-Pro.local>
2024-02-24 17:14:30 -0500

    changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ function Hero({ pageInfo}: Props) {
             <div className='z-20'>
                 <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>{pageInfo?.role}</h2>
                 <h1 className='text-5xl lg:text-5xl font-semibold scroll-px-10'>
-                    <span className='mr-3'>{text}</span>
+                    <span className='mr-3'>abc{text}</span>
                     <Cursor cursorColor='#F7AB0A' />
                 </h1>
                 <div className='pt-5'>
