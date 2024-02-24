98a687ac5df228d1f59908c0d4a5ccb6848fdfba
Rambod Zafarmoghaddam <rambodzafar@Rambods-MacBook-Pro.local>
2024-02-24 17:44:41 -0500

    changes

--- components/About.tsx
+++ components/About.tsx
@@ -4,12 +4,15 @@ import { motion } from 'framer-motion'
 import React from 'react'
 
 type Props = {
-    pageInfo: PageInfo
+    pageInfo: PageInfo,
+    isVisible: Boolean
 }
 
-function About({ pageInfo}: Props) {
+function About({ pageInfo, isVisible }: Props) {
     return (
-        <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration:1.5}} className='h-screen flex flex-col relative text-center md:text-left 
+    //     <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration:1.5}} className='h-screen flex flex-col relative text-center md:text-left 
+    // md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <motion.div animate={{opacity: isVisible ? 1:0 }} transition={{duration:1.5}} className='h-screen flex flex-col relative text-center md:text-left 
     md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
             <motion.img initial={{x:-200, opacity: 0}} src={urlFor(pageInfo?.profilePic).url()} transition={{duration: 1.2}} whileInView={{opacity: 1, x:0}} viewport={{once:true}} className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"   />
