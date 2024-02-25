124d012efa8b9a72f14bc4354dd246772eaa420f
Rambod Zafarmoghaddam <rambodzafar@Rambods-MacBook-Pro.local>
2024-02-25 12:46:33 -0500

    changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,7 @@ type Props = {
 function Projects({ projects}: Props) {
     return (
         <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration:1.5}}  className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
+            <h3 className='absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
             <div className='scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
                 {projects.map((project, i) => (
                     <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
