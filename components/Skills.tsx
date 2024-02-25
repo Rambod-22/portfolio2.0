3b86ddae245cc4986f0d1f93a66f43eb84bbb786
Rambod Zafarmoghaddam <rambodzafar@Rambods-MacBook-Pro.local>
2024-02-25 13:31:38 -0500

    changes

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,7 +14,7 @@ function Skills({skills}: Props) {
         <h3 className='absolute top-8 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
         </h3>
-        <h3 className='absolute top-16 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skills for current proficiency</h3>
+        <h3 className='absolute top-16 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current proficiency</h3>
         <div className='grid grid-cols-4 gap-5'>
           {skills?.slice(0, skills.length /2).map(skill => (
             <Skill key={skill._id} skill={skill}/>
